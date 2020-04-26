2037801bb48a41ff8ae44bd2d7d73506564e02e9
MoAli <ali@maahia.com>
2020-04-26 23:06:15 +0300

    commit

--- URL Open.py
+++ URL Open.py
@@ -1,6 +1,13 @@
+import re
 import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('http://xtools.com')
-
+fhand = urllib.request.urlopen('http://www.google.com')
+counts = dict()
 for line in fhand:
-    print(line.decode().strip())
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+print(counts)
+
+
