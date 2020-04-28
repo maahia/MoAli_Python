047cdf5f7fe3010d16c7b476939967819922a3b1
MoAli <ali@maahia.com>
2020-04-28 23:13:47 +0300

    commit

--- Bs4.py
+++ Bs4.py
@@ -3,18 +3,31 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
-
+url = "http://py4e-data.dr-chuck.net/known_by_Eugene.html"
+i = 7
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+print(url)
+for i in range(i):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tag = soup('a')
+    #print("current url is", url)
+    url = tag[17]
+    url = url.get('href', None)
+    print("the next url is", url)
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
+
+
+
+
 
 # Retrieve all of the anchor tags
-tags = soup('a')
-
-for tag in tags:
-    print(tag.get('href', None))
+# tag = soup('a')
+# url2 = tag[3]
+# url2 = url2.get('href', None)
+# html = urllib.request.urlopen(url2, context=ctx).read()
+# soup = BeautifulSoup(html, 'html.parser')
+# print(url2)
